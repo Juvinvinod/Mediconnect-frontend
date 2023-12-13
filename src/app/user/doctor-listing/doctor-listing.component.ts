7063dae830c7b2a1e41198a40e2631a91a0d4d4f
juvin vinod <124287134+Juvinvinod@users.noreply.github.com>
2023-12-13 15:25:14 +0530

    Added dept filtering

--- src/app/user/doctor-listing/doctor-listing.component.ts
+++ src/app/user/doctor-listing/doctor-listing.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AdminDoctorService } from 'src/app/admin/admin-doctor.service';
 import { Doctor } from 'src/app/shared/interfaces/doctor';
 import { Subscription } from 'rxjs';
+import { AdminService } from 'src/app/admin/admin.service';
+import { Dept } from 'src/app/shared/interfaces/department';
 
 @Component({
   selector: 'app-doctor-listing',
@@ -10,16 +12,40 @@ import { Subscription } from 'rxjs';
 })
 export class DoctorListingComponent implements OnInit, OnDestroy {
   doctorSubscription: Subscription | undefined = undefined;
+  deptSubscription: Subscription | undefined = undefined;
   searchValue = '';
+  selectedDept = '';
   doctorList: Doctor[] = [];
-  constructor(private adminService: AdminDoctorService) {}
+  filteredDoctorList: Doctor[] = [];
+  deptList: Dept[] = [];
+  constructor(
+    private adminDocService: AdminDoctorService,
+    private adminService: AdminService
+  ) {}
   ngOnInit(): void {
-    this.doctorSubscription = this.adminService.getDoctors().subscribe({
+    this.doctorSubscription = this.adminDocService.getDoctors().subscribe({
       next: (res) => {
         this.doctorList = res;
+        this.updateDoctors();
+      }
+    });
+    this.deptSubscription = this.adminService.getDepartments().subscribe({
+      next: (res) => {
+        this.deptList = res;
       }
     });
     window.scrollTo(0, 0);
+  }
+
+  updateDoctors() {
+    this.filteredDoctorList = this.doctorList.filter(
+      (doctor) =>
+        (this.searchValue === '' ||
+          doctor.first_name
+            .toLowerCase()
+            .includes(this.searchValue.toLowerCase())) &&
+        (this.selectedDept === '' || doctor.department === this.selectedDept)
+    );
   }
 
   trackById(index: number, doctor: Doctor) {
